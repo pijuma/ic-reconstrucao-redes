9faa999c7757a5ecd77ad9711472cd2a7a49dcf1
Pietra <pietragullo@gmail.com>
2026-02-01 13:28:57 -0300

    ultima vers

--- src/coupling.py
+++ src/coupling.py
@@ -16,60 +16,104 @@ alfa = 0.15
 b = 0.2
 c = 10
 w0 = 1 + 0.015
-w1 = 1 - 0.015
 #tem os valores iniciais do 1o e 2o acoplador já
-y0 = [-8, 8, 27, 0, 18, 27]
-
+X0 = np.random.uniform(-1, 1, size=(5, 3))
+delta = 0.02 
+w = w0 + delta*np.random.uniform(-1, 1, 5)
 
 dt = 0.01
-T = 2000
-
-num_pto = int(T/dt)
-
-t = np.linspace(0, T, num_pto)
+t0, tf = 0.0, 200.0
+T = np.arange(t0, tf, dt)
+K = 0.05 
 
 """
 Função que descreve o acoplamento de dois osciladores
 argumentos passados são:
-x: [x, y, z] -> define os pontos atuais
-w -> omega, para permitir uma diferença de fase
-x2 -> para o segundo oscilador, acoplando ele
-alfa, b, c, C -> parâmetros do modelo
-[x1, y1, z1, x2, y2, z2]
+
+X: (N, 3) - x, y, z pra cada nó 
+neig -> neighrnors, neig[i] = vizinhos do nó i 
+w -> vetor -> frequencia natural dos osciladores 
+alfa, b, c -> parametros do modelo 
+K -> força do acoplamento entre os osciladores
+
+retorna (N, 3) - 3 edos (x, y, z) pra cada nó
 """
-def rossler_acoplado(t, x, w1, w2, C):
-  dot = [(-w1*x[1] - x[2] + C*(x[3]-x[0])),
-         w1*x[0] + alfa*x[1],
-         b + x[2]*(x[0]-c),
-         (-w2*x[4] - x[5] + C*(x[0]-x[3])),
-         w2*x[3] + alfa*x[4],
-         b + x[5]*(x[3]-c)
-         ]
-  return np.array(dot)
+def rossler_rede(t, X, neig, K, w, alfa, b, c):
 
-#adaptando para ter os parametros do outro oscilador
-def runge_kutta(f, dt, y0, t0, w1, w2, C, t):
+  N = X.shape[0] #tamanho da rede 
+  dx = np.zeros_like(X) 
+  
+  for i in range(N):
+    xi, yi, zi = X[i] 
 
-  func = []
-  x_k = y0
-  func.append(x_k)
+    coup = 0.0 
+
+    for j in neig[i]:
+      coup += K*(X[j, 0] - xi) 
+    
+    dx[i, 0] = -w[i]*yi - zi 
+    dx[i, 1] = w[i]*xi + alfa*yi 
+    dx[i, 2] = b + zi*(xi-c)
+
+  return dx
+"""
+f -> função = rossler, lorrenz... 
+t -> tempo que irei avaliar 
+X -> valores iniciais 
+dt -> tamanho do passo 
+*args -> genérico para qualquer modelo, passo parametros
+"""
+
+def runge_kutta(f, t, X0, dt, *args):
+
+  X = X0.copy() #atual 
+  traj = [X.copy()]
 
   for i in range(len(t)-1):
-    f1 = f(t[i], x_k, w1, w2, C)
-    f2 = f(t[i]+(dt/2), x_k + (dt/2)*f1, w1, w2, C)
-    f3 = f(t[i]+(dt/2), x_k + (dt/2)*f2, w1, w2, C)
-    f4 = f(t[i]+dt, x_k + dt*f3, w1, w2, C)
-    x_k = x_k + (dt/6)*(f1 + 2*f2 + 2*f3 + f4)
-    func.append(x_k)
+    
+    f1 = f(t[i], X, *args)
+    f2 = f(t[i]+(dt/2), X + (dt/2)*f1, *args)
+    f3 = f(t[i]+(dt/2), X + (dt/2)*f2, *args)
+    f4 = f(t[i]+dt, X + dt*f3, *args)
 
-  return func
+    X = X + (dt/6)*(f1 + 2*f2 + 2*f3 + f4)
+    traj.append(X.copy())
+
+  return np.array(traj) #(T, N, 3) -> pra cada periodo no tempo, cada nó, cada coordenada
 
 """
-Testing 
+rede aleatória para testar
 """
-Y = runge_kutta(rossler_acoplado, dt, y0, 0, w0, w1, 0.027, t)
+N = 5 
+p = 0.5 
 
-ax = plt.figure().add_subplot(projection='3d')
-ax.plot([y[0] for y in Y], [y[1] for y in Y], [y[2] for y in Y], 'r', lw=0.5)
-#plt.show()
-plt.savefig(f"rossler_acoplado.png")
+G = nx.erdos_renyi_graph(N, p)
+neig = [list(G.neighbors(i)) for i in range(N)]
+print(neig)
+
+traj = runge_kutta(
+    rossler_rede,
+    t,
+    X0,
+    dt,
+    neig,
+    K,
+    w,
+    alfa,
+    b,
+    c
+)
+
+x_signals = traj[:, :, 0]# (T, N)
+
+x0 = x_signals[:, 0]# sinal do nó 0
+x1 = x_signals[:, 1]# sinal do nó 1
+
+plt.figure(figsize=(4,4))
+i = 0  # nó
+plt.plot(traj[:, i, 0], traj[:, i, 1], lw=0.5)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title(f'Retrato de fase – nó {i}')
+plt.tight_layout()
+plt.show()
